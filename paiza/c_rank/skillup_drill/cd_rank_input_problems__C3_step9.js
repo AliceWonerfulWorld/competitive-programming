8bf3dbb4c9399684f156586c0454cfbfa964c0dd
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-04-18 11:30:48 +0900

    cd_rank_input_problems__C3_step9

---
+++ paiza/c_rank/skillup_drill/cd_rank_input_problems__C3_step9.js
@@ -0,0 +1,25 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    console.log(Number(lines[0]) + 1);
+    for (let i = 1; i < lines.length; i++) {
+        let items = lines[i].trim().split(" ");
+        for (let j = 0; j < items.length; j++) {
+            if (isNaN(items[j])) {
+                console.log(items[j]);
+            } else {
+                console.log(Number(items[j]) + 1);
+            }
+        }
+    }
+});
