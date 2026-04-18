5f088b8e582c90418836f0c4f88b94173573784e
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-04-19 01:59:31 +0900

    square.js

---
+++ paiza/c_rank/skillup_drill/square.js
@@ -0,0 +1,20 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    let n = Number(lines[0]);
+    let result = "";
+    for (let i = 0; i < n; i++) {
+        result += "*";
+    }
+    console.log(result);
+});
