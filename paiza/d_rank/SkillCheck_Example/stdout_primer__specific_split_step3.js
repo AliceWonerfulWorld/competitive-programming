5c0e71afc4397dcfebd3c65ebbefea87c8f24da2
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-04-19 18:02:31 +0900

    stdout_primer__specific_split_step3

---
+++ paiza/d_rank/SkillCheck_Example/stdout_primer__specific_split_step3.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    let items = lines[0].trim().split(" ");
+    console.log(items.join(',') + ',');
+});
