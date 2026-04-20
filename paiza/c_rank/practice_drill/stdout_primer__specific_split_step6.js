fa635127743d3cad285f513e7be5dbfe59430f1c
AliceWonerfulWorld <gomaazarasi1789@gmail.com>
2026-04-20 15:39:55 +0900

    specific_split_step6.js

---
+++ paiza/c_rank/practice_drill/stdout_primer__specific_split_step6.js
@@ -0,0 +1,19 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+reader.on('line', (line) => {
+  lines.push(line);
+});
+reader.on('close', () => {
+    const N = lines[0].trim();
+
+    const result = N.match(/.{3}/g).join(',');
+
+    console.log(result);
+});
